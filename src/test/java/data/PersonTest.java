5d5728852538b03f10d8997cc16fdf4940745e69
Ismael <ismaelpastor2003@gmail.com>
2022-05-16 17:50:48 +0200

    Initial commit

---
+++ src/test/java/data/PersonTest.java
@@ -0,0 +1,20 @@
+package data;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getName()
+    {
+        Person p = new Person("James");
+        assertEquals("James", p.getName());
+    }
+}
